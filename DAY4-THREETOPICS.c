3bf5feb8ace80b822268bcacc87f70bcfc547e6d
ayush27-bit <ayush.rajput270406@gmail.com>
2024-10-01 20:03:11 +0530

    Add files via upload

---
+++ DAY4-THREETOPICS.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+int a,b,c,x;
+        scanf("%d %d %d %d",&a,&b,&c,&x);
+        if(x==a || x==b || x==c)
+        printf("YES \n");
+        else
+        printf("NO \n");
+        
+	return 0;
+}
+

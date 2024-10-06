6170c1b72c020c7b495e5073e2fd23fc5ca75c0b
ayush27-bit <ayush.rajput270406@gmail.com>
2024-10-06 14:55:19 +0530

    Add files via upload

---
+++ DAY9-WATERCOOLER2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    if(x<y && y%x!=0){
+	    printf("%d\n",y/x);}
+	    else if(y>x && y%x==0){
+	        printf("%d\n",(y/x)-1);
+	    }
+	    else{
+	        printf("0\n");
+	    }
+	}
+	return 0;
+}
+

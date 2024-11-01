2a9760ffd69c711e06f11dc3b98392957becede1
ayush27-bit <ayush.rajput270406@gmail.com>
2024-11-01 17:01:02 +0530

    Add files via upload

---
+++ DAY30-TRUESCORE.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int t,a,b,c,d;
+	
+	scanf("%d",&t);
+	
+	while(t--)
+	
+	{
+	    scanf("%d %d %d %d",&a,&b,&c,&d);
+	    
+	    if(a<=c && b<=d)
+	    
+	    printf("POSSIBLE\n");
+	    
+	    else
+	    
+	    printf("IMPOSSIBLE\n");
+	    
+	}
+	
+	return 0;
+}
+

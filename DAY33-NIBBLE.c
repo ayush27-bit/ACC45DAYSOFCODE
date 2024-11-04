7149104f74b36ed5483366805d91234998bcb0fb
ayush27-bit <ayush.rajput270406@gmail.com>
2024-11-04 20:38:05 +0530

    Add files via upload

---
+++ DAY33-NIBBLE.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int t,n;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&n);
+	    if(n%4==0)
+	    printf("Good\n");
+	    else
+	    printf("Not Good\n");
+	}
+	return 0;
+}
+
